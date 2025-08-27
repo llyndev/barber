5d7379068f70261b83b5ab0079685744865cca5e
llyndev <weslley.alves3310@gmail.com>
2025-08-27 19:08:55 -0300

    feat(UserService): find by id method

--- src/main/java/com/barbearia/barbearia/service/UserService.java
+++ src/main/java/com/barbearia/barbearia/service/UserService.java
@@ -1,11 +1,15 @@
 package com.barbearia.barbearia.service;
 
 import com.barbearia.barbearia.dto.request.UserRequest;
+import com.barbearia.barbearia.dto.response.UserResponse;
+import com.barbearia.barbearia.mapper.UserMapper;
 import com.barbearia.barbearia.model.AppUser;
 import com.barbearia.barbearia.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -15,9 +19,21 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserMapper userMapper;
 
     public List<AppUser> findAll() {
         return userRepository.findAll();
+    }
+
+    public UserResponse getById(Long id) {
+        return userRepository.findById(id)
+                .map(userMapper::toDTO)
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+    }
+
+    public AppUser getEntityById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
     }
 
     public AppUser register(UserRequest userRequest) {
