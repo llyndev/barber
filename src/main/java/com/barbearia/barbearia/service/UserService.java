08e3fb5de1d6b0e8a3ef35b66d156cee1ab6e466
llyndev <weslley.alves3310@gmail.com>
2025-08-31 22:24:18 -0300

    refactor(UserService): move registration methods to new service

--- src/main/java/com/barbearia/barbearia/service/UserService.java
+++ src/main/java/com/barbearia/barbearia/service/UserService.java
@@ -1,13 +1,11 @@
 package com.barbearia.barbearia.service;
 
-import com.barbearia.barbearia.dto.request.UserRequest;
 import com.barbearia.barbearia.dto.response.UserResponse;
 import com.barbearia.barbearia.mapper.UserMapper;
 import com.barbearia.barbearia.model.AppUser;
 import com.barbearia.barbearia.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -18,7 +16,6 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
     private final UserMapper userMapper;
 
     public List<AppUser> findAll() {
@@ -34,25 +31,5 @@ public class UserService {
     public AppUser getEntityById(Long id) {
         return userRepository.findById(id)
                 .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
-    }
-
-    public AppUser register(UserRequest userRequest) {
-        if (!userRequest.getPassword().equals(userRequest.getConfirmPassword())) {
-            throw new IllegalArgumentException("Invalid credential");
-        }
-
-        if (userRepository.findByEmail(userRequest.getEmail()).isPresent()) {
-            throw new IllegalArgumentException("Email already exists");
-        }
-
-        AppUser user = AppUser.builder()
-                .name(userRequest.getName())
-                .email(userRequest.getEmail())
-                .telephone(userRequest.getTelephone())
-                .password(passwordEncoder.encode(userRequest.getPassword()))
-                .role(AppUser.Role.CLIENT)
-                .build();
-
-        return userRepository.save(user);
     }
 }
