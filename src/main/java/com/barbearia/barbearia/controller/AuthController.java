5c2c95b95d35d9d8597602c7ea60a89d80554e26
llyndev <weslley.alves3310@gmail.com>
2025-09-11 19:49:05 -0300

    feat: added get /me method to get user information

--- src/main/java/com/barbearia/barbearia/controller/AuthController.java
+++ src/main/java/com/barbearia/barbearia/controller/AuthController.java
@@ -2,17 +2,19 @@ package com.barbearia.barbearia.controller;
 
 import com.barbearia.barbearia.dto.request.AuthRequest;
 import com.barbearia.barbearia.dto.response.AuthResponse;
+import com.barbearia.barbearia.mapper.UserMapper;
+import com.barbearia.barbearia.model.AppUser;
 import com.barbearia.barbearia.security.JwtUtil;
+import com.barbearia.barbearia.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -21,6 +23,7 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
+    private final UserMapper userMapper;
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest authRequest) {
@@ -33,5 +36,15 @@ public class AuthController {
         String token = jwtUtil.generateToken(userDetails);
 
         return ResponseEntity.ok(new AuthResponse(token));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<AppUser> getMe(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        if (userDetails == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        AppUser user = userDetails.user();
+
+        return ResponseEntity.ok(user);
     }
 }
