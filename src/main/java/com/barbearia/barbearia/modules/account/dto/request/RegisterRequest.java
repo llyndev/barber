b8446efb9f91af07b1f363151fa7965cf84e7786
llyndev <weslley.alves3310@gmail.com>
2026-01-25 13:23:43 -0300

    feat: add password size validation and permit all access to forgot/reset password endpoints

--- src/main/java/com/barbearia/barbearia/modules/account/dto/request/RegisterRequest.java
+++ src/main/java/com/barbearia/barbearia/modules/account/dto/request/RegisterRequest.java
@@ -2,6 +2,7 @@ package com.barbearia.barbearia.modules.account.dto.request;
 
 import com.barbearia.barbearia.modules.account.model.AppUser;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record RegisterRequest(
         @NotBlank
@@ -13,7 +14,7 @@ public record RegisterRequest(
         @NotBlank
         String telephone,
 
-        @NotBlank
+        @NotBlank @Size(min = 8, max = 72)
         String password,
 
         @NotBlank
