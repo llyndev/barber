120ed9e70ec2037cbb82f8fbc08986cfff91b42a
llyndev <weslley.alves3310@gmail.com>
2026-09-03 21:36:00 -0300

    feat: adicionado novas exceptions

---
+++ src/main/java/com/barbearia/barbearia/exception/PlanLimitExceededException.java
@@ -0,0 +1,7 @@
+package com.barbearia.barbearia.exception;
+
+public class PlanLimitExceededException extends RuntimeException {
+    public PlanLimitExceededException(String message) {
+        super(message);
+    }
+}
